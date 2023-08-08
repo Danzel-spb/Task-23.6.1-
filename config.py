b34dd8ec2cd12b5022e7d9d948df2fabb3ced21a
Danzel-spb <132992726+Danzel-spb@users.noreply.github.com>
2023-08-08 09:56:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '6688859713:AAGkj24WNvIszz0bw20KwOSEftovMOoIiqM'
+CURRENCY_API = 'cb22fe54c2fbf452ce66c6beeb301689f1b642417e23181041484341285f6f52'
+#AVALIABLE CURRENCY
+val = {
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
+    'доллар' : 'USD'
+}
+symbols = {
+    'EUR' : '€',
+    'RUB' : '₽',
+    'USD' : '$'
+}
